673fc7e90268c467f2e5263be93d600a2f22341e
rexthonyy <rexthonyy@gmail.com>
2020-08-26 10:44:55 +0100

    Nav bar list item changes color

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -7,13 +7,39 @@ function scrollListener(){
 }
 
 function notifyNavBarListener(){
+	var navBar = getNavBar();
+	var inactiveNavItems = getInactiveNavItems();
+
 	if(document.body.scrollTop > 10 || document.documentElement.scrollTop > 10){
-		getNavBar().className="nav_translucent";
+		navBar.className="nav_translucent";
+		
+		for(var i = 0; i < inactiveNavItems.length; i++){
+			inactiveNavItems[i].className = "nav_item_inactive_translucent";
+		}
 	}else{
-		getNavBar().className="nav_transparent";
+		navBar.className="nav_transparent";
+
+		for(var i = 0; i < inactiveNavItems.length; i++){
+			inactiveNavItems[i].className = "nav_item_inactive_transparent";
+		}
 	}
 }
 
 function getNavBar(){
 	return document.getElementById("nav");
+}
+
+function getInactiveNavItems(){
+	var inactiveNavItems = [];
+	var inactiveNavItems1 = document.getElementsByClassName("nav_item_inactive_transparent");
+	var inactiveNavItems2 = document.getElementsByClassName("nav_item_inactive_translucent");
+
+	for(var i = 0; i < inactiveNavItems1.length; i++){
+		inactiveNavItems.push(inactiveNavItems1[i]);
+	}
+	for(var i = 0; i < inactiveNavItems2.length; i++){
+		inactiveNavItems.push(inactiveNavItems2[i]);
+	}
+	
+	return inactiveNavItems;
 }
