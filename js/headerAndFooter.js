b3a1db6d9e469101eb3f196a200a41da10224ee9
rexthonyy <rexthonyy@gmail.com>
2020-08-28 16:48:51 +0100

    Started creating the header image responsive design

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -8,17 +8,21 @@ function scrollListener(){
 
 function notifyNavBarListener(){
 	var navBar = getNavBar();
+	var navItemContainer = getNavItemContainer();
 	var inactiveNavItems = getInactiveNavItems();
+	var navHamburger = getNavHamburger();
 
 	if(document.body.scrollTop > 10 || document.documentElement.scrollTop > 10){
 		navBar.className="nav_translucent";
-		
+		navHamburger.className = "checkbtn_translucent";
+		navItemContainer.className = "nav_item_container_translucent";
 		for(var i = 0; i < inactiveNavItems.length; i++){
 			inactiveNavItems[i].className = "nav_item_inactive_translucent";
 		}
 	}else{
 		navBar.className="nav_transparent";
-
+		navHamburger.className = "checkbtn_transparent";
+		navItemContainer.className = "nav_item_container_transparent";
 		for(var i = 0; i < inactiveNavItems.length; i++){
 			inactiveNavItems[i].className = "nav_item_inactive_transparent";
 		}
@@ -28,7 +32,9 @@ function notifyNavBarListener(){
 function getNavBar(){
 	return document.getElementById("nav");
 }
-
+function getNavItemContainer(){
+	return document.getElementById("nav_item_container");
+}
 function getInactiveNavItems(){
 	var inactiveNavItems = [];
 	var inactiveNavItems1 = document.getElementsByClassName("nav_item_inactive_transparent");
@@ -42,4 +48,8 @@ function getInactiveNavItems(){
 	}
 	
 	return inactiveNavItems;
+}
+
+function getNavHamburger(){
+	return document.getElementById("nav_hamburger");
 }
