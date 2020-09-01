29dfa4e07103adfd88b64861990131b395a23d0a
rexthonyy <rexthonyy@gmail.com>
2020-09-01 18:09:09 +0100

    Implemented the about responsive but not the javascript. Will work on it this night... Probably

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -24,8 +24,10 @@ function windowClickListener(){
 		checkbox.checked = true;
 	}
 }
+
 function scrollListener(){
 	notifyNavBarListener();
+	notifyNavItemListener();
 }
 
 function notifyNavBarListener(){
@@ -49,6 +51,53 @@ function notifyNavBarListener(){
 			inactiveNavItems[i].className = "nav_item_inactive_transparent";
 		}
 	}
+}
+
+function notifyNavItemListener(){
+	var index = undefined;
+	for(var i = 0; i < getSection().length; i++){
+		var section = getSection()[i].getBoundingClientRect();
+		if(section.top > 0){
+			index = i;
+			break;
+		}
+	}
+	if(index != undefined){
+		disableAllNavHeaderItems();
+		setActiveNavHeader(index);
+	}
+}
+
+function disableAllNavHeaderItems(){
+	var activeItem  = document.getElementsByClassName("nav_item_active")[0].className = "nav_item_inactive_translucent";
+}
+
+function setActiveNavHeader(index){
+	switch(index){
+		case 1: //home
+			document.getElementById("nav_item_home").className = "nav_item_active";
+		break;
+
+		case 2: //home
+			document.getElementById("nav_item_about").className = "nav_item_active";
+		break;
+
+		case 3: //services
+			document.getElementById("nav_item_services").className = "nav_item_active";
+		break;
+
+		case 4: //Portfolio
+			document.getElementById("nav_item_portfolio").className = "nav_item_active";
+		break;
+
+		case 5: //Pricing
+			document.getElementById("nav_item_pricing").className = "nav_item_active";
+		break;
+	}
+}
+
+function getSection(){
+	return document.getElementsByTagName("section");
 }
 
 function getCheckbox(){
