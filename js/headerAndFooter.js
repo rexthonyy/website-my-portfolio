fe0902dcd3a7f634c8959627cc4f1d8d46ec3ef1
rexthonyy <rexthonyy@gmail.com>
2020-09-01 21:22:02 +0100

    Completed the about section responsive design

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -2,6 +2,7 @@ window.onload = function(){
 	window.onscroll = scrollListener;
 	window.onclick = windowClickListener;
 	getNavHamburger().onclick = windowClickListener;
+	loadSkills();
 }
 
 function windowClickListener(){
@@ -65,11 +66,12 @@ function notifyNavItemListener(){
 	if(index != undefined){
 		disableAllNavHeaderItems();
 		setActiveNavHeader(index);
+		notifyNavBarListener();
 	}
 }
 
 function disableAllNavHeaderItems(){
-	var activeItem  = document.getElementsByClassName("nav_item_active")[0].className = "nav_item_inactive_translucent";
+	var activeItem = document.getElementsByClassName("nav_item_active")[0].className = "nav_item_inactive_translucent";
 }
 
 function setActiveNavHeader(index){
@@ -127,4 +129,40 @@ function getInactiveNavItems(){
 
 function getNavHamburger(){
 	return document.getElementById("nav_hamburger");
+}
+
+function loadSkills(){
+	var skills = [];
+	skills.push(new Skill("UI/UX Design", 83));
+	skills.push(new Skill("Web Design", 92));
+	skills.push(new Skill("App development", 87));
+	skills.push(new Skill("SEO", 32));
+
+	loadSkillLayout(skills);
+}
+
+function loadSkillLayout(skills){
+	var skillElements = getSkillElements();
+	for(var i = 0; i < skillElements.length; i++){
+		var innerHTML = "";
+		var skillElm = skillElements[i];
+		for(var j = 0; j < skills.length; j++){
+			var skill = skills[j];
+			
+			innerHTML += "<h3>"+skill.name+"</h3><h4>"+skill.percent+"%</h4><div class=\"skill_bar_background\"><div style=\"width:"+skill.percent+"%\" class=\"skill_bar_progress\"></div></div>";
+		}
+		skillElm.innerHTML = innerHTML;
+	}
+}
+
+function Skill(name, percent){
+	this.name = name;
+	this.percent = percent;
+}
+
+function getSkillElements(){
+	var skillElements = [];
+	skillElements.push(document.getElementById("skills"));
+	skillElements.push(document.getElementById("skills2"));
+	return skillElements;
 }
