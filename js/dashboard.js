d40663a8ff5a96716a3218e5afa614898ee8f9cf
rexthonyy <rexthonyy@gmail.com>
2020-09-07 00:07:01 +0100

    Completed the dataless structure, next is data binding

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,10 @@ window.onload = function(){
 }
 
 function setEventListeners(){
+	setCloseSelectionClickListener();
+	setPublishSelectedButtonClickListener();
+	setUnpublishSelectedButtonClickListener();
+	setDeleteSelectedButtonClickListener();
 	setNewPostListener();
 	setSearchBarFocusListener();
 	setSearchBarChangeListener();
@@ -10,6 +14,34 @@ function setEventListeners(){
 	setSearchButtonListener();
 	setDisplayPostDropDownSelectionListener();
 	setSelectAllCheckboxistener();
+	setPostHoverListener();
+	setPostCheckboxClickListener();
+	setPublishOrUnpublishPostButtonListener();
+	setDeletePostButtonListener();
+	setPreviewPostButtonListener();
+	setSharePostButtonListener();
+	setAnalyticsButtonListener();
+	setPostClickListener();
+}
+
+function setCloseSelectionClickListener(){
+	var closeSelectionBtn = getCloseSelectionButton();
+	closeSelectionBtn.onclick = clickListener;
+}
+
+function setPublishSelectedButtonClickListener(){
+	var publishSelectedBtn = getPublishSelectedButton();
+	publishSelectedBtn.onclick = clickListener;
+}
+
+function setUnpublishSelectedButtonClickListener(){
+	var unpublishSelectedBtn = getUnpublishSelectedButton();
+	unpublishSelectedBtn.onclick = clickListener;
+}
+
+function setDeleteSelectedButtonClickListener(){
+	var deleteSelectedBtn = getDeleteSelectedButton();
+	deleteSelectedBtn.onclick = clickListener;
 }
 
 function setNewPostListener(){
@@ -62,6 +94,127 @@ function setSelectAllCheckboxistener(){
 	};
 }
 
+function setPostHoverListener(){
+	var postItems = getPostItems();
+
+	for(var i = 0; i < postItems.length; i++){
+		var menuItems = getMenuItems();
+		var menuIcons = getMenuIcons();
+		menuItems[i].style.display = "none";
+		menuIcons[i].style.display = "block";
+		postItems[i].addEventListener("mouseover", function(){
+			var menuItems = getMenuItems();
+			var menuIcons = getMenuIcons();
+			menuItems[this.id].style.display = "block";
+			menuIcons[this.id].style.display = "none";
+		});
+		postItems[i].addEventListener("mouseout", function(){
+			var menuItems = getMenuItems();
+			var menuIcons = getMenuIcons();
+			menuItems[this.id].style.display = "none";
+			menuIcons[this.id].style.display = "block";
+		});
+	}
+}
+
+function setPostCheckboxClickListener(){
+	var postCheckboxes = getPostCheckboxes();
+	for(var i = 0; i < postCheckboxes.length; i++){
+		postCheckboxes[i].onclick = function(e){
+			stopClickPropagation(e);
+		};
+		postCheckboxes[i].onchange = function(event){
+			checkSelectedPosts();
+		}
+	}
+}
+function setPublishOrUnpublishPostButtonListener(){
+	var btns = getPublishOrUnpublishPostButton();
+	for(var i = 0; i < btns.length; i++){
+		btns[i].onclick = function(e){
+			stopClickPropagation(e);
+			alert("Publish post");
+		};
+	}
+}
+
+function setDeletePostButtonListener(){
+	var btns = getDeletePostButton();
+	for(var i = 0; i < btns.length; i++){
+		btns[i].onclick = function(e){
+			stopClickPropagation(e);
+			alert("Delete post");
+		};
+	}
+}
+
+function setPreviewPostButtonListener(){
+	var btns = getPreviewPostButton();
+	for(var i = 0; i < btns.length; i++){
+		btns[i].onclick = function(e){
+			stopClickPropagation(e);
+			alert("Preview post");
+		};
+	}
+}
+
+function setSharePostButtonListener(){
+	var btns = getSharePostButtons();
+	for(var i = 0; i < btns.length; i++){
+		btns[i].onclick = function(e){
+			stopClickPropagation(e);
+			alert("Share post");
+		};
+	}
+}
+
+function setAnalyticsButtonListener(){
+	var btns = getAnalyticsPostButtons();
+	for(var i = 0; i < btns.length; i++){
+		btns[i].onclick = function(e){
+			stopClickPropagation(e);
+			alert("Analytics post");
+		};
+	}
+}
+
+function setPostClickListener(){
+	var posts = getPosts();
+	for(var i = 0; i < posts.length; i++){
+		posts[i].onclick = function(e){
+			alert("Clicked post with id : " + this.id);
+		};
+	}
+}
+
+function getSearchNav(){
+	return document.getElementById("searchNav");
+}
+
+function getSelectionNav(){
+	return document.getElementById("selectionNav");
+}
+
+function getCloseSelectionButton(){
+	return document.getElementById("closeSelectionBtn");
+}
+
+function getPublishSelectedButton(){
+	return document.getElementById("publishSelectedBtn");
+}
+
+function getUnpublishSelectedButton(){
+	return document.getElementById("unpublishSelectedBtn");
+}
+
+function getDeleteSelectedButton(){
+	return document.getElementById("deleteSelectedBtn");
+}
+
+function getNumSelection(){
+	return document.getElementById("numSelection");
+}
+
 function getNewPostButtons(){
 	return document.getElementsByClassName("btnNewPost");
 }
@@ -90,9 +243,66 @@ function getSelectAllCheckbox(){
 	return document.getElementById("selectAllCheckbox");
 }
 
+function getPostItems(){
+	return document.getElementsByClassName("post");
+}
+
+function getMenuItems(){
+	return document.getElementsByClassName("menuItems");
+}
+
+function getMenuIcons(){
+	return document.getElementsByClassName("menuIcon");
+}
+
+function getPostCheckboxes(){
+	return document.getElementsByClassName("postCheckbox");
+}
+
+function getPublishOrUnpublishPostButton(){
+	return document.getElementsByClassName("publishBtn");
+}
+
+function getDeletePostButton(){
+	return document.getElementsByClassName("deleteBtn");
+}
+
+function getPreviewPostButton(){
+	return document.getElementsByClassName("previewBtn");
+}
+
+function getSharePostButtons(){
+	return document.getElementsByClassName("shareBtn");
+}
+
+function getAnalyticsPostButtons(){
+	return document.getElementsByClassName("analyticsBtn");
+}
+
+function getPosts(){
+	return document.getElementsByClassName("post");
+}
+
 function clickListener(event){
 	var element = event.target;
 	switch(element.id){
+		case "closeSelectionBtn":
+			deselectAllPosts();
+			checkSelectedPosts();
+		break;
+
+		case "publishSelectedBtn":
+			alert("publish selected button");
+		break;
+
+		case "unpublishSelectedBtn":
+			alert("unpublish selected button");
+		break;
+
+		case "deleteSelectedBtn":
+			alert("delete selected button");
+		break;
+
 		case "newPostBtn":
 			// window.open("editor.php", "_self");
 			alert("create a new post");
@@ -131,6 +341,15 @@ function searchBarChangeListener(value){
 	}
 }
 
+function stopClickPropagation(e){
+	if(!e) e = window.event;
+	if(e.stopPropagation){
+		e.stopPropagation();
+	}else{
+		e.cancelBubble = true;
+	}
+}
+
 function displayPosts(){
 	var searchBar = getSearchBar();
 	var displayPostDropDown = getDisplayPostDropDown();
@@ -138,13 +357,56 @@ function displayPosts(){
 }
 
 function selectAllPosts(){
-	alert("Selecting all posts");
+	var postCheckboxes = getPostCheckboxes();
+	for(var i = 0; i < postCheckboxes.length; i++){
+		postCheckboxes[i].checked = true;
+	}
 }
 
 function deselectAllPosts(){
-	alert("Deselecting all posts");
+	var postCheckboxes = getPostCheckboxes();
+	for(var i = 0; i < postCheckboxes.length; i++){
+		postCheckboxes[i].checked = false;
+	}
 }
 
 function checkSelectedPosts(){
-	alert("Checking selected posts");
+	checkAPostSelected();
+	checkAllPostsSelected();
+}
+
+function checkAPostSelected(){
+	var searchNav = getSearchNav();
+	var selectionNav = getSelectionNav();
+	var postCheckboxes = getPostCheckboxes();
+	var numSelection = getNumSelection();
+	var isAPostSelected = false;
+	var numPostsSelected = 0;
+	for(var i = 0; i < postCheckboxes.length; i++){
+		if(postCheckboxes[i].checked){
+			isAPostSelected = true;
+			numPostsSelected++;
+		}
+	}
+	if(isAPostSelected){
+		searchNav.style.display = "none";
+		selectionNav.style.display = "block";
+		numSelection.textContent = numPostsSelected + " selected";
+	}else{
+		searchNav.style.display = "block";
+		selectionNav.style.display = "none";
+	}
+}
+
+function checkAllPostsSelected(){
+	var selectAllCheckbox = getSelectAllCheckbox();
+	var postCheckboxes = getPostCheckboxes();
+	var isAllPostsSelected = true;
+	for(var i = 0; i < postCheckboxes.length; i++){
+		if(!postCheckboxes[i].checked){
+			isAllPostsSelected = false;
+			break;
+		}
+	}
+	selectAllCheckbox.checked = isAllPostsSelected;
 }
